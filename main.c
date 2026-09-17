22aa9fc61e3ca2f7cab170bc64da13744a0ea580
methmi punsara <methmipunsara04@gmail.com>
2026-09-17 15:00:03 +0530

    Added specialty lookup arrays

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 
 #define MAXPATIENTS 100
 
+const float SPECIALTYFEE[4] = {1500.0, 2500.0, 4500.0, 5000.0};
+const int SPECIALTYTIME[4] = {15, 20, 30, 30};
+const int SPECIALTYCAP[4] = {30, 20, 12, 10};
+
+
 int main() {
     printf("Smart Hospital Management System ...\n");
     return 0;
 }
+
