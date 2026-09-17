2579b69d4f06eca8ade63f06d95918650310da26
methmi punsara <methmipunsara04@gmail.com>
2026-09-17 16:49:07 +0530

    Add lookup tables for specialties, daily caps and wards

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 
 #define MAXPATIENTS 100
 
-const float SPECIALTYFEE[4] = {1500.0, 2500.0, 4500.0, 5000.0};
-const int SPECIALTYTIME[4] = {15, 20, 30, 30};
-const int SPECIALTYCAP[4] = {30, 20, 12, 10};
+const char SPECIALTY_NAMES[4][30] = {"General Practice", "Paediatrics", "Cardiology", "Neurology"};
+const float BASE_FEES[4] = {1500.0, 2500.0, 4500.0, 5000.0};
+const int AVG_TIMES[4] = {15, 20, 30, 30};
+const int DAILY_CAPS[4] = {30, 20, 12, 10};
 
+const char WARD_NAMES[4][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
+const float WARD_RATES[4] = {3000.0, 6000.0, 12000.0, 25000.0};
+const int WARD_CAPACITY[4] = {20, 10, 10, 5};
 
 int main() {
     printf("Smart Hospital Management System ...\n");
