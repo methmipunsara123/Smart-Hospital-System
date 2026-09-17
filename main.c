ad2b1189fb3cfe2f823ba4e50bd08443df31723e
methmi punsara <methmipunsara04@gmail.com>
2026-09-17 21:32:23 +0530

    Implement basic patient registration inputs and daily capacity check

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 #define MAXPATIENTS 100
 
-const char SPECIALTY_NAMES[4][30] = {"General Practice", "Paediatrics", "Cardiology", "Neurology"};
-const float BASE_FEES[4] = {1500.0, 2500.0, 4500.0, 5000.0};
-const int AVG_TIMES[4] = {15, 20, 30, 30};
-const int DAILY_CAPS[4] = {30, 20, 12, 10};
+const char SPECIALTYNAMES[4][30] = {"General Practice", "Paediatrics", "Cardiology", "Neurology"};
+const float BASEFEES[4] = {1500.0, 2500.0, 4500.0, 5000.0};
+const int AVGTIMES[4] = {15, 20, 30, 30};
+const int DAILYCAPS[4] = {30, 20, 12, 10};
 
-const char WARD_NAMES[4][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
-const float WARD_RATES[4] = {3000.0, 6000.0, 12000.0, 25000.0};
-const int WARD_CAPACITY[4] = {20, 10, 10, 5};
+const char WARDNAMES[4][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
+const float WARDRATES[4] = {3000.0, 6000.0, 12000.0, 25000.0};
+const int WARDCAPACITY[4] = {20, 10, 10, 5};
 
 int bedOccupancy[4][20];
 
@@ -35,9 +35,47 @@ int assignedBed[MAXPATIENTS];
 int patientCount = 0;
 int waitingCounts[4] = {0, 0, 0, 0};
 
+void registerPatient() {
+    if(patientCount >= MAXPATIENTS) {
+        printf("System full!\n");
+        return;
+    }
+
+    int specIndex;
+    printf("\nSelect Specialty (1-OPD, 2-Paediatrics, 3-Cardiology, 4-Neurology): ");
+    scanf("%d", &specIndex);
+
+    if(waitingCounts[specIndex - 1] >= DAILYCAPS[specIndex - 1]) {
+        printf("Daily limit reached for this specialty!\n");
+        return;
+    }
+
+    patientID[patientCount] = 1001 + patientCount;
+    specialtyID[patientCount] = specIndex;
+
+
+    printf("Enter Patient Name: ");
+    getchar();
+    fgets(patientName[patientCount], sizeof(patientName[patientCount]), stdin);
+    patientName[patientCount][strcspn(patientName[patientCount], "\n")] = 0;
+
+
+    printf("Enter Age: ");
+    scanf("%d", &patientAge[patientCount]);
+
+    printf("Enter Urgency Level (1-Normal, 2-Urgent, 3-Critical): ");
+    scanf("%d", &urgencyLevel[patientCount]);
+
+    waitingCounts[specIndex - 1]++;
+    patientCount++;
+    printf("\n[Reg-%d] Registered successfully!\n", patientID[patientCount - 1]);
+
+}
+
 int main() {
     setupBeds();
     printf("Smart Hospital Management System ...\n");
+    registerPatient();
     return 0;
 }
 
